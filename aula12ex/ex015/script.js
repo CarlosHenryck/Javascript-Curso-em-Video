a1aeffe9c748a9bcee2b9390decb883b2fc48558
Henryck <ryck302@gmail.com>
2020-11-15 00:20:17 -0300

    atualizar exercicio

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -11,13 +11,35 @@ function verificar() {
         var idade = ano - Number(fano.value)
         var gênero = ''
         var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
 
         if (fsex[0].checked) {
             gênero = 'Homem' 
+
+            if (idade >= 0 && idade <= 10) {
+                img.setAttribute('src', 'foto-bebe-m.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'foto-jovem-m.png')
+            } else if (idade <= 50) {
+                img.setAttribute('src', 'foto-adulto-m.png')
+            } else {
+                img.setAttribute('src', 'foto-idoso-m.png')
+            }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
+
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'foto-bebe-f.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'foto-jovem-f.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'foto-adulto-f.png')
+            } else {
+                img.setAttribute('src', 'foto-idoso-f.png')
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Dectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
